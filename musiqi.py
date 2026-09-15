18f777f7eff86bd58abd573243bf89b74fc25ff4
RahimCr7 <rehim947@gmail.com>
2026-09-16 01:45:44 +0400

    Create music database in musiqi.py
    Added a music database with various genres and example links.

---
+++ musiqi.py
@@ -0,0 +1,20 @@
+# 🎵 BURA BÜTÜN MAHNILARIN TELEGRAM KODLARINI YIĞIRIQ
+# Hər janrın içinə 100-dənə də kod daxil etsən, vergüllə alt-alta yaza bilərsən!
+
+mahni_bazasi = {
+    "bass": [
+        "CQACAgIAAxkBAAO3aqm5Rn2q3FeCXHDNqIk-s6RPX9wAAqugAAKYJrFIkxCMomUByoo9BA" # Nümunə link
+    ],
+    "azeri": [
+        "https://soundhelix.com" # Sənin indicə aldığın ilk real kod!
+    ],
+    "rus": [
+        "https://soundhelix.com"
+    ],
+    "turk": [
+        "https://soundhelix.com"
+    ],
+    "meyxana": [
+        "https://soundhelix.com"
+    ]
+}
